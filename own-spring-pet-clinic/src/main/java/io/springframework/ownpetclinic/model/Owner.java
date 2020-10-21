c98f51cbf74cfa1454bf24d19486d0166a478a6d
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-10-21 19:36:22 -0300

    added - POJOs basic model

---
+++ own-spring-pet-clinic/src/main/java/io/springframework/ownpetclinic/model/Owner.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.ownpetclinic.model;
+
+/**
+ *
+ * @author msulbara
+ */
+public class Owner extends Person {
+
+}
---
+++ own-spring-pet-clinic/src/main/java/io/springframework/ownpetclinic/model/Pet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.ownpetclinic.model;
+
+/**
+ *
+ * @author msulbara
+ */
+public class Pet {
+
+    private String name;
+
+    private PetType petType;
+
+    private Owner owner;
+
+    private LocalDate birthDate;
+
+}
