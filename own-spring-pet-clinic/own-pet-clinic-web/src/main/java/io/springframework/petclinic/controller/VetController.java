6cb2bcd7f4b04ee8a5ddbd1a095373360aa1c127
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-10-25 17:38:54 -0300

    vets index and controllers, Closes #7.

---
+++ own-spring-pet-clinic/own-pet-clinic-web/src/main/java/io/springframework/petclinic/controller/VetController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author msulbara
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+
+}
