a3e37ef55657d788920d9df32b0797e2e18d5bf4
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-11-11 20:46:49 -0300

    petTypeMapService Implementation, Closes #29

---
+++ own-spring-pet-clinic/own-pet-clinic-data/src/main/java/io/springframework/petclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.petclinic.service.map;
+
+import io.springframework.petclinic.model.PetType;
+import io.springframework.petclinic.service.PetTypeService;
+import java.util.Set;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author msulbara
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
