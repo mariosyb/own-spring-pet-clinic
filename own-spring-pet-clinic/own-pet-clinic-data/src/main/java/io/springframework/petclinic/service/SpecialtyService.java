2b88cf6964d6e074ee1da387a3c5926ac5182447
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-11-14 17:02:34 -0300

    added speciality map service impl, Closes #31

---
+++ own-spring-pet-clinic/own-pet-clinic-data/src/main/java/io/springframework/petclinic/service/SpecialtyService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.petclinic.service;
+
+import io.springframework.petclinic.model.Speciality;
+
+/**
+ *
+ * @author msulbara
+ */
+public interface SpecialtyService extends CrudService<Speciality, Long>{
+    
+}
---
+++ own-spring-pet-clinic/own-pet-clinic-data/src/main/java/io/springframework/petclinic/service/map/SpecialityMapService.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.petclinic.service.map;
+
+import io.springframework.petclinic.model.Speciality;
+import io.springframework.petclinic.service.SpecialtyService;
+import java.util.Set;
+
+/**
+ *
+ * @author msulbara
+ */
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
