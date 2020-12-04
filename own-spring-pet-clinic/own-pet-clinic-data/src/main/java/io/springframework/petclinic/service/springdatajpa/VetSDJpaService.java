ce322029aed929fe944f43d5d05219cf4da275ca
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-12-03 21:33:07 -0300

    Closes #41, Vet Service Implemented

---
+++ own-spring-pet-clinic/own-pet-clinic-data/src/main/java/io/springframework/petclinic/service/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.petclinic.service.springdatajpa;
+
+import io.springframework.petclinic.model.Vet;
+import io.springframework.petclinic.repositories.VetRepository;
+import io.springframework.petclinic.service.VetService;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author msulbara
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+
+}
