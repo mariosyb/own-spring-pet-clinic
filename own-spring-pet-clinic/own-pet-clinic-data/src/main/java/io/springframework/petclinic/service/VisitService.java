ca65053bcc804f13a731e8ce3ac2aa067ce1b959
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-12-07 14:23:38 -0300

    Closes #46, added Visit map service

---
+++ own-spring-pet-clinic/own-pet-clinic-data/src/main/java/io/springframework/petclinic/service/VisitService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.petclinic.service;
+
+import io.springframework.petclinic.model.Visit;
+
+/**
+ *
+ * @author msulbara
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
