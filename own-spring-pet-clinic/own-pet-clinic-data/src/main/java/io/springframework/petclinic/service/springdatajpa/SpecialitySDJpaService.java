f2f84941e38ad94af608f2842cff39cc29632fb5
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-12-07 13:52:43 -0300

    Closes #44, Specialty Service Impl added

---
+++ own-spring-pet-clinic/own-pet-clinic-data/src/main/java/io/springframework/petclinic/service/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.springframework.petclinic.service.springdatajpa;
+
+import io.springframework.petclinic.model.Speciality;
+import io.springframework.petclinic.repositories.SpecialtyRepository;
+import io.springframework.petclinic.service.SpecialtyService;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author msulbara
+ */
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialitySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+
+}
