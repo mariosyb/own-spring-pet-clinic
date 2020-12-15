5716a8317948fd237fdad5bdf1ad738353cf2d03
msulbara <msulbara@MSULBARA-AR.ar.oracle.com>
2020-12-15 18:28:48 -0300

    JUnit 5 Already in SpringBoot Test Starter, Closes #4

---
+++ own-spring-pet-clinic/own-pet-clinic-web/src/test/java/io/springframework/petclinic/OwnPetClinicApplicationIT.java
@@ -0,0 +1,18 @@
+package io.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Integration Test
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class OwnPetClinicApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+}
