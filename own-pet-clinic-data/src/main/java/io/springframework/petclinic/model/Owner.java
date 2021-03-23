a7caa486b01ac311a8d8b36a78250d98f4976a68
msul <mario.sulbaran@vusecurity.com>
2021-03-23 18:47:13 -0300

    adding PetTypeFormatter, #56.

--- own-pet-clinic-data/src/main/java/io/springframework/petclinic/model/Owner.java
+++ own-pet-clinic-data/src/main/java/io/springframework/petclinic/model/Owner.java
@@ -25,6 +25,19 @@ import javax.persistence.Table;
 @Table(name = "owners")
 public class Owner extends Person {
 
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+
+        if (pets != null) {
+            this.pets = pets;
+        }
+    }
+
     @Column(name = "address")
     private String address;
 
@@ -34,17 +47,38 @@ public class Owner extends Person {
     @Column(name = "telephone")
     private String telephone;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner") // one to many requiere mappedBy
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    @Builder
-    public Owner(Long id, String firstName, String lastName,
-                 String address, String city, String telephone, Set<Pet> pets) {
-        super(id, firstName, lastName);
-        this.address = address;
-        this.city = city;
-        this.telephone = telephone;
-        this.pets = pets;
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    /**
+     * Return the Pet with the given name, or null if none found for this Owner.
+     *
+     * @param name to test
+     * @return true if pet name is already in use
+     */
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (Pet pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
     }
 
 }
--- own-pet-clinic-data/src/main/java/io/springframework/petclinic/model/Pet.java
+++ own-pet-clinic-data/src/main/java/io/springframework/petclinic/model/Pet.java
@@ -6,6 +6,7 @@
 package io.springframework.petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -25,10 +26,22 @@ import javax.persistence.Table;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
+    }
 
     @Column(name = "name")
     private String name;
@@ -38,13 +51,13 @@ public class Pet extends BaseEntity {
     private PetType petType;
 
     @ManyToOne
-    @JoinColumn(name = "owner_id") // manyToOne requires joinColumn
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
-
 }
