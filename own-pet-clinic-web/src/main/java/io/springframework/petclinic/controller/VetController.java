4b667ed948ed5a5021de2a14240986a0600e0463
msul <mario.sulbaran@vusecurity.com>
2021-03-25 18:47:52 -0300

    closes #62, closes#74; Vets liss view and vets JSON endpoint

--- own-pet-clinic-web/src/main/java/io/springframework/petclinic/controller/VetController.java
+++ own-pet-clinic-web/src/main/java/io/springframework/petclinic/controller/VetController.java
@@ -5,10 +5,15 @@
  */
 package io.springframework.petclinic.controller;
 
+import io.springframework.petclinic.model.Vet;
 import io.springframework.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  *
@@ -17,18 +22,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-    final VetService vetService;
+    private final VetService vetService;
 
     public VetController(VetService vetService) {
         this.vetService = vetService;
     }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
-    public String listVets(Model model) {
-        
+    public String listVets(Model model){
+
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 
 }
